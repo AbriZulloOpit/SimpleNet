4897840d0ef2fe8146ae3dbbc0e712be83736004
AbriZulloOpit <gabriele.z@students.opit.com>
2026-09-19 14:07:08 +0000

    added training bottle categories and modified bash command

---
+++ run_bottle_test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+datapath=/content/drive/MyDrive/CapstoneProject/mvtec_anomaly_detection
+
+datasets=('bottle')
+
+dataset_flags=($(for dataset in "${datasets[@]}"; do echo '-d '"${dataset}"; done))
+
+python3 main.py \
+--gpu 0 \
+--seed 0 \
+--log_group simplenet_mvtec_bottle_test \
+--log_project MVTecAD_Results \
+--results_path results \
+--run_name bottle_test_run \
+net \
+-b wideresnet50 \
+-le layer2 \
+-le layer3 \
+--pretrain_embed_dimension 1536 \
+--target_embed_dimension 1536 \
+--patchsize 3 \
+--meta_epochs 20 \
+--embedding_size 256 \
+--gan_epochs 4 \
+--noise_std 0.015 \
+--dsc_hidden 1024 \
+--dsc_layers 2 \
+--dsc_margin .5 \
+--pre_proj 1 \
+dataset \
+--batch_size 8 \
+--resize 329 \
+--imagesize 288 "${dataset_flags[@]}" mvtec $datapath
